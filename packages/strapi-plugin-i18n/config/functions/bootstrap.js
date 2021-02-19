459e0b957146fbddca4bcbf7a2995d43660f4478
Convly <jean-sebastien.herbaux@epitech.eu>
2021-02-19 20:05:37 +0100

    (tmp) permissions engine rework: fields => properties + domain layer

--- packages/strapi-plugin-i18n/config/functions/bootstrap.js
+++ packages/strapi-plugin-i18n/config/functions/bootstrap.js
@@ -16,9 +16,48 @@ const DEFAULT_LOCALE = {
   code: 'en-US',
 };
 
+const conditions = [
+  {
+    displayName: 'Has Locale Access',
+    name: 'has-locale-access',
+    plugin: 'i18n',
+    handler: (user, options) => {
+      const {
+        properties: { locales = [] },
+      } = options;
+
+      return {
+        'locale.code': {
+          $in: locales,
+        },
+      };
+    },
+  },
+];
+
+/**
+ *
+ * @param {Permission} permission
+ */
+const i18nPermissionHandler = permission => {
+  const {
+    subject,
+    properties: { locales = [] },
+  } = permission.raw;
+
+  // If there is a subject & the subject has i18n enabled & there is some locale restrictions
+  if (subject && locales.length > 0) {
+    permission.addCondition('plugins::i18n.has-locale-access');
+  }
+};
+
 module.exports = async () => {
-  const { actionProvider } = strapi.admin.services.permission;
+  const { actionProvider, conditionProvider, engine } = strapi.admin.services.permission;
+
   actionProvider.register(actions);
+  conditionProvider.registerMany(conditions);
+
+  engine.registerPermissionsHandler(i18nPermissionHandler);
 
   const defaultLocale = await getService('locales').getDefaultLocale();
   if (!defaultLocale) {
